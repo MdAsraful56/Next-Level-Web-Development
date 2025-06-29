b9eb8866e12ede8159ecaf3ac0faff89dcadfb1a
MdAsraful56 <work.mdasraful56@gmail.com>
2025-06-29 14:49:41 +0600

    2 day preter code extention problem solving

--- Module - 22/src/module/tasks/AddTaskModal.tsx
+++ Module - 22/src/module/tasks/AddTaskModal.tsx
@@ -63,6 +63,52 @@ export function AddTaskModal() {
                                         </FormControl>
                                     </FormItem>
                                 )} />
+                                <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="dob"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    captionLayout="dropdown"
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
                             <DialogFooter className="mt-5">
                                 <DialogClose asChild>
                                     <Button variant="outline">Cancel</Button>
