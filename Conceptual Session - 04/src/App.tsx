b26c26c53eafc342c7c049e4ba6e1b0985a0c0d9
MdAsraful56 <work.mdasraful56@gmail.com>
2025-07-01 19:43:29 +0600

    conceptual session class - 04

---
+++ Conceptual Session - 04/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="">
+      <Button className="bg-blue-500 text-white hover:bg-blue-600">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
