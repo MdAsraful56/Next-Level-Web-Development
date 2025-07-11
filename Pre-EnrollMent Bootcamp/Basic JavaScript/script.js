c1ee7fc4fa49f3f491c2ca9ccc506d63d7cdc982
MdAsraful56 <work.mdasraful56@gmail.com>
2025-07-12 00:05:27 +0600

    ..

---
+++ Pre-EnrollMent Bootcamp/Basic JavaScript/script.js
@@ -0,0 +1,4 @@
+console.log('Hello World!');
+// This is a simple JavaScript file that logs "Hello World!" to the console.
+// You can run this code in a browser console or a JavaScript environment to see the output.
+// Feel free to modify this code and experiment with JavaScript.
